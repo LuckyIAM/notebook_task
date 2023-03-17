081d47ca50ac6b309c7e2f4f3833c531db6c5e72
LuckyIAM <zavalisca.elena@gmail.com>
2023-03-18 00:51:54 +0300

    changed little

--- main.js
+++ main.js
@@ -3,13 +3,25 @@ import data from './data.json' assert { type: "json" };
 let table = document.querySelector(".table")
 let body = document.body
 const statusValue = ["work", "vacation"]
+const week = ["пн","вт","ср","чт","пт","сб","вс"]
+const dayOfMarch = []
+let saveLast;
+
+
+function generateMonth(start, end){
+    for(let i = start; i < end; i++){
+        dayOfMarch.push(i)
+    }
+    return dayOfMarch
+}
+generateMonth(6, 31)
 
 // Создание Таблицы
 function createTable(parent, n, number){
     for(let i = 0; i < n; i++){
         let newCell = document.createElement('div')
-        newCell.innerText = `${ i + 1}`
-        newCell.classList.add(`child${number}_${i}`, "draw-cell")
+        newCell.innerHTML = `<b>${week[i]}</b><sub>${dayOfMarch[i]}</sub>`
+        newCell.classList.add(`march_${dayOfMarch[i]}_${number}`, "draw-cell")
         parent.appendChild(newCell)
     }
 }
@@ -18,16 +30,14 @@ function createTable(parent, n, number){
 function createRow(){
     const newRow = document.createElement("div")
     newRow.classList.add("draw-row")  
-    let count = 0
     for(let i = 0; i<data.length; i++){
-        count++
         const firstCell= document.createElement("div")
-        firstCell.classList.add(`first_child${count}_${i}`, "draw-cell")
+        firstCell.classList.add(`first_child_${i}`, "draw-head-cell")
         firstCell.innerText = `${data[i].name}`
         newRow.appendChild(firstCell)
-        createTable(newRow, 15, count)
+        createTable(newRow, 7, i)
     }
-    body.appendChild(newRow)
+    table.appendChild(newRow)
 }
 createRow()
 
@@ -39,23 +49,32 @@ function fillTable(){
                 if(obj[j].status !== statusValue[1]){
                     const start = +obj[j].start.split('.')[0]
                     const end = +obj[j].end.split('.')[0] 
-                    console.log(start, end);
+                    const busyDays = end - start + 1;  
+                    const nameTiket = obj[j].tiket
                     for(let q = start; q <= end; q++){                  
-                        const bg = document.querySelector(`.child${i+1}_${q-1}`)
+                        const bg = document.querySelector(`.march_${q}_${i}`)
                         console.log(bg);
-                        if(bg){ //&& !bg.classList.contains("color-green")
+                        if(bg && !bg.classList.contains(`color-green`)){ 
                             bg.classList.add("color-green")
+                            const saveText = bg.innerHTML
+                            bg.innerHTML = `${nameTiket}_${saveText} `
+                        }else{
+                            console.log("It's busy!"); //данный блок надо менять для добавление задачи
+                            break;  
                         }
                     }
                 }else if(obj[j].status === statusValue[1]){
                     const start = +obj[j].start.split('.')[0]
                     const end = +obj[j].end.split('.')[0] 
-                    console.log(start, end);
+                    // console.log(start, end);
                     for(let q = start; q <= end; q++){                  
-                        const bg = document.querySelector(`.child${i+1}_${q-1}`)
-                        console.log(bg);
-                        if(bg){
+                        const bg = document.querySelector(`.march_${q}_${i}`)
+                        // console.log(bg);
+                        if(bg && !bg.classList.contains(`color-red`)){
                             bg.classList.add("color-red")
+                        }else{
+                            console.log("It's busy!"); //данный блок надо менять для добавление задачи
+                            break; // предупреждение !!
                         }
                     }
                 }
@@ -64,3 +83,5 @@ function fillTable(){
     }
 }
 fillTable()
+
+
